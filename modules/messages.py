26ed9a281faec3e64cc80e2e96f2ab5a23b10fa5
Vinícius Souza <vinissou.git@gmail.com>
2024-10-02 00:31:33 -0300

    add

---
+++ modules/messages.py
@@ -0,0 +1,37 @@
+help_text = " Use the --options flag to show all available options"
+
+options_text = """
+
+Listing all available formats...
+Use the following names to select the paper size:
+
+    ISO A    ISO B    ISO C    ISO D    ISO RA/SRA
+
+      A0      B0       C0       D0         SRA0
+      A1      B1       C1                  SRA1
+      A2      B2       C2                  SRA2
+      A3      B3       C3                  SRA3
+      A4      B4       C4                  SRA4
+      A5      B5       C5                  RA0
+      A6      B6       C6                  RA1
+      A7      B7                           RA2
+      A8      B8
+              B10
+
+
+
+
+    ANSI       UK PAPER     UK PRESS    Newspaper 
+
+    Letter         
+    Legal
+    Ledger 
+    Tabloid 
+    Executive
+    ANSI_C
+    ANSI_D
+    ANSI_E
+ 
+"""
+
+# print(options_text)
